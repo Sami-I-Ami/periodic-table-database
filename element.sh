be7f7793ea65af361c57ec573540a93f9901ef60
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-16 23:59:08 +0000

    feat: output for no argument

--- element.sh
+++ element.sh
@@ -3,3 +3,8 @@
 # variable to run PSQL
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+# check if argument given
+if [[ -z $1 ]]
+then
+  echo Please provide an element as an argument.
+fi
