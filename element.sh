fefc5a34bf7e24d062e60a214adb859a6bc7ab9f
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-17 00:14:09 +0000

    feat: returning output when info doesn't exist

--- element.sh
+++ element.sh
@@ -5,14 +5,17 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # check if argument given
 if [[ -z $1 ]]
-  then
-    echo Please provide an element as an argument.
+then
+  echo Please provide an element as an argument.
 
 # check if argument is a number
 elif [[ $1 =~ ^[0-9]+$ ]]
-  then
-    # get info from database
-    ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) WHERE atomic_number = $1")
+then
+  # get info from database
+  ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) WHERE atomic_number = $1")
 
-    # if it doesn't exist
+  # if it doesn't exist
+  if [[ -z $ELEMENT_INFO ]]
+  then
+    echo I could not find that element in the database.
 fi
