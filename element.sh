442a53c4ca18e1f074995c2567d412d227120590
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-17 00:44:23 +0000

    feat: added check for name

--- element.sh
+++ element.sh
@@ -45,4 +45,22 @@ then
      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     done
   fi
+
+# check for name
+else
+  # get info from database
+  ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name = '$1'")
+
+  # if it doesn't exist
+  if [[ -z $ELEMENT_INFO ]]
+  then
+    echo I could not find that element in the database.
+  
+  else
+    # read info
+    echo "$ELEMENT_INFO" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE
+    do
+     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
+  fi
 fi
