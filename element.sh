746c2a6447554815d327a477685863db3a480b71
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-17 00:05:12 +0000

    feat: check for number argument

--- element.sh
+++ element.sh
@@ -5,6 +5,11 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # check if argument given
 if [[ -z $1 ]]
-then
-  echo Please provide an element as an argument.
+  then
+    echo Please provide an element as an argument.
+
+# check if argument is a number
+elif [[ $1 =~ ^[0-9]+$ ]]
+  then
+    echo This is a number
 fi
